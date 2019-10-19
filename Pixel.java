acfa6ae886bef3672dc4ba7fc8e80ebaf68dc2ce
JD110225 <51008292+JD110225@users.noreply.github.com>
2019-10-19 11:32:17 -0600

    Add files via upload

---
+++ Pixel.java
@@ -0,0 +1,21 @@
+
+public class Pixel
+{
+   int fila;
+   int columna;
+   public Pixel(int fila,int columna){
+       this.fila=fila;
+       this.columna=columna;
+    }
+   public int getFila(){
+       return fila;
+   }
+   public int getColumna(){
+       return columna;
+   }
+   public String toString(){
+       String tira="";
+       tira+="Fila: "+getFila()+"\n"+"Columna: "+getColumna()+"\n";
+       return tira;
+    }
+}
