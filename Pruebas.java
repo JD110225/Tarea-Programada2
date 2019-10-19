4b30478a5ae7cb576f9d9b8d27c1eabb26cb7fd8
JD110225 <51008292+JD110225@users.noreply.github.com>
2019-10-19 11:33:03 -0600

    Add files via upload

---
+++ Pruebas.java
@@ -0,0 +1,53 @@
+public class Pruebas
+{
+   private Imagen imagen;
+   private int[][] matrizImagen;
+   private int[][] matrizImagenNueva; 
+   public Pruebas(){
+       matrizImagen=new int[][]{{1,1,1,2,2,1,1,1,1,1},
+                                {1,1,2,1,2,1,1,1,1,1}};
+       matrizImagenNueva=new int[matrizImagen.length][matrizImagen[0].length];
+       for(int f=0;f<matrizImagen.length;++f){
+           for(int c=0;c<matrizImagen[0].length;++c){       
+               matrizImagenNueva[f][c]=-1;//Hacer matriz de puros pixeles blancos
+            }
+        } 
+}
+   public boolean existePixel(int fila,int columna){
+       return fila>=0 && columna>=0 && fila<matrizImagen.length && columna<matrizImagen[0].length;
+    }    
+   //Recibe posicion de un pixel y retorna vector de Pixeles que son vecinos e iguales en valor.
+   public Pixel[] vecinosIguales(int fila,int columna){ 
+       Pixel[] pixelesVecinos=new Pixel[8];
+       int contadorFilas=0;
+       int[] sumaF={ -1,-1, 0, 1, 1, 1, 0,-1 };
+       int[] sumaC={  0, 1, 1, 1, 0,-1,-1,-1 };
+       for(int i=0;i<8;++i){
+           int filaComparar=fila+sumaF[i];
+           int columnaComparar=columna+sumaC[i];
+           if(existePixel(filaComparar,columnaComparar)){
+                Pixel pixelVecino=new Pixel(filaComparar,columnaComparar);
+                if(matrizImagen[filaComparar][columnaComparar]==matrizImagen[fila][columna]){
+                    pixelesVecinos[contadorFilas++]=pixelVecino;               
+                }
+   }
+}   
+        return pixelesVecinos;
+}
+    public Pixel findBorde(){
+        Pixel fondo=new Pixel(0,0);
+        Pixel borde=fondo; 
+        boolean encontrado=false;
+        for(int f=0;f<matrizImagen.length;f++){
+           for(int c=0;!encontrado&&c<matrizImagen[0].length;c++){
+               if(matrizImagen[f][c]!=matrizImagen[0][0]){
+                   borde=new Pixel(f,c);
+                   encontrado=true;
+                }
+    }
+    }
+        return borde;
+    }
+}
+    
+
