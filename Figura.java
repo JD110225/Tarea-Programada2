ad741e38b37382d246c6a68e4a3c4291527967e7
JD110225 <51008292+JD110225@users.noreply.github.com>
2019-10-22 17:03:22 -0600

    Update Figura.java

--- Figura.java
+++ Figura.java
@@ -1,17 +1,24 @@
 public class Figura
 {
    private int[][] matriz;
+   private Imagen dibujo;
    private int areaPixeles;
    private int cantidadManchas;
    private int[] dimensiones;
    private Pixel borde;
    private Pixel colorFondo;
    private int escala;
+   /*
+    * este constructor lo usaremos mas adelante creo...
    public Figura(int[][]matriz,Pixel borde,Pixel colorFondo){
        this.matriz=matriz;
        this.borde=borde;
        this.colorFondo=colorFondo;
    }
+   */
+   public Figura(int[][] matriz){
+       this.matriz=matriz;
+    }
    public int getAreaPixeles(){
       return areaPixeles;
    }
@@ -21,5 +28,19 @@ public class Figura
    public int getEscala(){
        return escala;
     }
-   
+   public int[][] getMatriz(){
+       return matriz;
+    }
+   public void displayMatriz(){
+        for(int f=0;f<matriz.length;++f){
+            for(int c=0;c<matriz[0].length;++c){
+                System.out.print(matriz[f][c]+" ");
+            }
+            System.out.println();
+        }
+    }
+   public void verDibujo(){
+       dibujo=new Imagen(matriz);
+       dibujo.dibujar();
+    }
 }
