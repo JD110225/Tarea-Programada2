247cdedf6d3fe92755d75852ce30468c44243c8c
JD110225 <51008292+JD110225@users.noreply.github.com>
2019-10-22 17:07:11 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main
+{
+   public static void main(String[] args){
+       Pruebas prueba=new Pruebas();
+       Figura figura=prueba.crearFigura();
+       figura.verDibujo();
+    }
+}
