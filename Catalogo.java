3dd285bece2f44472baf147e99fac21ce3222ef4
JD110225 <51008292+JD110225@users.noreply.github.com>
2019-10-24 17:48:24 -0600

    Add files via upload

--- Catalogo.java
+++ Catalogo.java
@@ -1,9 +1,19 @@
 
 public class Catalogo{
-    private Figura [] figura;
-    
+    private Figura[] figuras;
+    private int indiceCatalogo;
     public Catalogo(){
-
+        figuras=new Figura[100];
     }
-
+    public void agregarFigura(Figura figura){
+        figuras[indiceCatalogo++]=figura;
+    }
+    public void verCatalogo(){
+        for(Figura f:figuras){
+            if(f!=null){
+            f.verDibujo();
+        }
+    }
+    }
 }
+
