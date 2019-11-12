a27bcd15cf83e3fb11ee0d6c5c2f48cc7d9de540
KABU09 <55645024+KABU09@users.noreply.github.com>
2019-11-11 22:28:25 -0600

    Add files via upload

--- Interfaz.java
+++ Interfaz.java
@@ -1,22 +1,29 @@
 import javax.swing.JOptionPane;
-public class Interfaz
-{
-   public int pedirEntero(String mensaje){
-       int entero=-1;
-       boolean revision=false;
-       while(!revision){
-           try{
-           String enteroString=JOptionPane.showInputDialog(mensaje);
-           entero=Integer.parseInt(enteroString);
-           revision=true;
-        }
-           catch(Exception e){
-               JOptionPane.showMessageDialog(null,"No se digito un valor valido","Error",JOptionPane.ERROR_MESSAGE);
+public class Interfaz{
+    
+    public int pedirEntero(String mensaje){
+        int entero=-1;
+        boolean revision=false;
+        while(!revision){
+            try{
+                String enteroString=JOptionPane.showInputDialog(mensaje);
+                entero=Integer.parseInt(enteroString);
+                revision=true;
             }
+            catch(Exception e){
+                mostrarError("No se digito un valor valido");
+                entero = -1;
+            }
+        }
+        return entero;
     }
-    return entero;
-}
+    
+    public void mostrarError(String mensaje){
+        JOptionPane.showMessageDialog(null,mensaje,"Error",JOptionPane.ERROR_MESSAGE);
+    }
+    
     public void mostrarMensaje(String mensaje){
         JOptionPane.showMessageDialog(null,mensaje);
     }
+    
 }
